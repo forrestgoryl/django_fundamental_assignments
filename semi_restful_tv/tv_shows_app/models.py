e46bb56d6feb0859470be2bb10f06dd1ea445b88
forrestgoryl <forrestgoryl@outlook.com>
2021-03-19 08:05:37 +0800

    added validation to semi_restful_tv

--- semi_restful_tv/tv_shows_app/models.py
+++ semi_restful_tv/tv_shows_app/models.py
@@ -1,4 +1,40 @@
 from django.db import models
+import re, datetime
+
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        DATE_REGEX = re.compile(r"\d\d\d\d-\d\d-\d\d")
+        title_list = []
+        date = datetime.datetime.now()
+        year = date.strftime("%Y")
+        month = date.strftime("%m")
+        day = date.strftime("%d")
+        for show in Show.objects.all():
+            title_list.append(show.title)
+        errors = {}
+        if len(postData['title']) < 2:
+            errors['title'] = "The title you entered was too short. Show titles should be longer, shouldn't they?"
+        if postData['title'] in title_list:
+            errors['title'] = "The title you entered was the same as a show already entered before. Please select a different movie to enter."
+        if len(postData['network']) < 3:
+            errors['network'] = "The network name you entered was too short. No networks go by only 2 or fewer characters."
+        if not DATE_REGEX.match(postData['release']):
+            errors['release'] = "The release date you entered doesn't have the right format. Please enter your release date like so: YYYY-MM-DD."
+        if int(postData['release'][0:4]) > int(year):
+            errors['release'] = "The release date you entered is in the future."
+        if int(postData['release'][0:4]) == int(year):
+            if int(postData['release'][5:7]) > int(month):
+                errors['release'] = "The release date you entered is in the future."
+            elif int(postData['release'][5:7]) == int(month):
+                if int(postData['release'][8:10]) > int(day):
+                    errors['release'] = "The release date you entered is in the future."
+                else:
+                    pass
+            else:
+                pass
+        if len(postData['desc']) < 10:
+            errors['desc'] = "Please make your description longer."
+        return errors
 
 class Show(models.Model):
     title = models.CharField(max_length=45)
@@ -7,3 +43,4 @@ class Show(models.Model):
     desc = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
--- semi_restful_tv/tv_shows_app/views.py
+++ semi_restful_tv/tv_shows_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponse
 from tv_shows_app.models import Show
+from django.contrib import messages
+import re
 
 def shows(request):
     object_list = Show.objects.all()
@@ -21,35 +23,50 @@ def shows_id(request, show_number):
 def shows_new(request):
     return render(request, 'shows_new.html')
 
-def shows_create(request):
-    _title = request.POST['title']
-    _network = request.POST['network']
-    _release = request.POST['release']
-    _desc = request.POST['desc']
-    Show.objects.create(title=_title, network=_network, release=_release, desc=_desc)
-    return redirect("/")
-
 def shows_edit(request, show_number):
     context = {
         'show_number': show_number
     }
     return render(request, 'shows_edit.html', context)
 
+def create(request):
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect("/shows/new")
+    else:
+        _title = request.POST['title']
+        _network = request.POST['network']
+        _release = request.POST['release']
+        _desc = request.POST['desc']
+        Show.objects.create(title=_title, network=_network, release=_release, desc=_desc)
+        return redirect("/shows")
+
 def update(request, show_number):
-    show = Show.objects.get(id=show_number)
-    if 'title' in request.POST != '':
-        show.title = request.POST['title']
-    if 'network' in request.POST != '':
-        show.network = request.POST['network']
-    if 'release' in request.POST != '':
-        show.release = request.POST['release']
-    if 'desc' in request.POST != '':
-        show.desc = request.POST['desc']
-    show.save()
-    return redirect("/")
+    errors = Show.objects.basic_validator(request.POST)
+    if 'title' in errors.keys():
+        if errors['title'] == "The title you entered was the same as a show already entered before. Please select a different movie to enter.":
+            if len(errors) == 1:
+                errors = {}
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f"/shows/{show_number}/edit")
+    else:
+        show = Show.objects.get(id=show_number)
+        if 'title' in request.POST != '':
+            show.title = request.POST['title']
+        if 'network' in request.POST != '':
+            show.network = request.POST['network']
+        if 'release' in request.POST != '':
+            show.release = request.POST['release']
+        if 'desc' in request.POST != '':
+            show.desc = request.POST['desc']
+        show.save()
+        return redirect("/shows")
 
 def delete(request, show_number):
-    print("The button is being clicked")
     show = Show.objects.get(id=show_number)
     show.delete()
-    return redirect("/")
+    return redirect("/shows")
